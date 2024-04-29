291deb6fb81d6b288da909f1ce234cb5e95ff71f
John Bosworth <john@jo4nny8.co.uk>
2024-04-29 12:57:31 +0100

    Update custom-meta.php

--- custom-meta.php
+++ custom-meta.php
@@ -36,15 +36,14 @@ if ( empty( $sections ) ) {
 
 		<?php if ( 'categories' == $section ) { ?>
 			
-					<?php
-						$terms = get_the_terms($post->ID, 'installation-categories' );
-		          if ($terms && ! is_wp_error($terms)) :
-		            $term_slugs_arr = array();
-		              foreach ($terms as $term) {
-		              	$term_slugs_arr[] = $term->slug;
-		              }
-		                $terms_slug_str = join( " ", $term_slugs_arr);
-		              
+			<?php
+			$terms = get_the_terms($post->ID, 'installation-categories' );
+			if ($terms && ! is_wp_error($terms)) :
+			    $term_slugs_arr = array();
+			      foreach ($terms as $term) {
+				$term_slugs_arr[] = $term->slug;
+			      }
+			$terms_slug_str = join( " ", $term_slugs_arr);           
                   ?>
                   <li class="meta-cat">
                   <i class="icon-folder"></i>
